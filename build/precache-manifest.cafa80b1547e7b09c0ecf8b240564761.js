8b45aa756aa08466503cc5f2503c1ee313555c93
sulveena <67237902+sulveena@users.noreply.github.com>
2020-06-24 18:31:48 +0530

    Created

---
+++ build/precache-manifest.cafa80b1547e7b09c0ecf8b240564761.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7e249502aa3d07a377818b5a42c9b4d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b54ea4b1d1573f2f4b10",
+    "url": "/static/css/2.3cb76441.chunk.css"
+  },
+  {
+    "revision": "d6bddd66f8904a791bee",
+    "url": "/static/css/main.86d9a5ed.chunk.css"
+  },
+  {
+    "revision": "b54ea4b1d1573f2f4b10",
+    "url": "/static/js/2.6e492a31.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.6e492a31.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6bddd66f8904a791bee",
+    "url": "/static/js/main.7a0e3801.chunk.js"
+  },
+  {
+    "revision": "8f2c9cc0412125cd7310",
+    "url": "/static/js/runtime-main.03c88bfe.js"
+  }
+]);
